8d7a08a1782e63c3c2da9d4cda40bf9c5d3b2e7d
zakkdruzer <zakkdruzer@gmail.com>
2026-08-16 23:08:39 -0300

    docs: documentar y preparar entrega final del módulo 8

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/m8-proy-final-clima-vuex/',
+
   plugins: [vue()],
 
   resolve: {
